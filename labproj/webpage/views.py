bade6394a14bfdb162011634e80533dfbc6490c8
A3ayedh <145850892+A3ayedh@users.noreply.github.com>
2023-10-26 15:55:19 +0300

    abdullahlab26

--- labproj/webpage/views.py
+++ labproj/webpage/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def webpage_view(request):
-    x = HttpResponse('Hello World')
-    return x
+    data = {}
+    data['abdullah'] = 'abady'
+    return render (request, 'abdullah.html', context=data)
