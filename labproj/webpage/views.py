f3174e4bd9a0ad84447e8ed474f9eba346ff0310
BaderJAlmuzayen <146174495+BaderJAlmuzayen@users.noreply.github.com>
2023-10-27 14:00:23 +0300

    submitting the edits

--- labproj/webpage/views.py
+++ labproj/webpage/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def webpage_view(request):
-    
     context = {
-        " name " : "Bader" , 
-        " Classes" : " MIS350"
-    
+        "name": "Bader",
+        "Classe": "MIS350",
+        "Professors_List": ["Dr. Ali", "Dr. Mariam", "Dr. Mohammad"],
+        "Courses_Grades": [
+            {"Course": " MIS 240", "Grade": "B+"},
+            {"Course": " MIS 120", "Grade": "B-"},
+            {"Course": " Accounting 1", "Grade": "F"}
+        ]
     }
     
-    return render (request, 'Bader.html', context)
+    return render(request, 'Bader.html', context)
