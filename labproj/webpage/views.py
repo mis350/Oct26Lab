35dad627c373fd54b750b05f3a87bcb5be98a8b9
Shahad-Meshal <shahad.meshwet@cba.ku.edu.kw>
2023-10-28 20:36:34 +0300

    ended

--- labproj/webpage/views.py
+++ labproj/webpage/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
+
 def webpage_view(request):
-    x = HttpResponse('Hello World')
-    return x
+    context = {
+        "Name": "Shahad",
+        "Major": "mis",
+        "Courses_List": ["mis", "finanace", "e-commerce", "acc1"],
+        "Courses_Grades": [
+            {"Course": "mis",          "Grade":"B"},
+            {"Course": "finance",      "Grade":"B+"},
+            {"Course": "e-commerce",   "Grade":"A"},
+            {"Course": "acc1",         "Grade":"A"}
+        ]
+    }
+    
+    return render(request, 'shahad.html', context)
+
+
+
